9a219c45e66c9083c8a657e15f37f113fe8d8269
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2023-03-13 16:33:13 +0000

    Bump version v0.3.0 -> v0.4.0

--- hatch_requirements_txt/__init__.py
+++ hatch_requirements_txt/__init__.py
@@ -43,7 +43,7 @@ __all__ = ("RequirementsMetadataHook", "parse_requirements")
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2022 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.3.0"
+__version__: str = "0.4.0"
 __email__: str = "dominic@davis-foster.co.uk"
 
 # Regular expression for matching comments at the end of requirements
